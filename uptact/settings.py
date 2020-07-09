b06c1b070715cf92f9bd9155af308c2d117e030e
Robert Nsiah <robert.kofi.nsiah@gmail.com>
2020-07-09 14:14:09 +0000

    checking

--- uptact/settings.py
+++ uptact/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-uptact-rnsiah--kofinsiah.repl.co']
+ALLOWED_HOSTS = ['django-uptact-rnsiah-kofinsiah.repl.co']
 
 # Application definition
 
